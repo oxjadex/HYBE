dac2b7b16e3a42a5de23fcfb6bc68d3aa579a8cb
oxjadex <oharin765@gmail.com>
2024-06-21 13:51:40 +0900

    feat: 1차 드롭다운

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-function showVideo() {
-  var wrapper = document.getElementById("video-wrapper");
-  var thumbnail = document.getElementById("thumbnail");
+document.getElementById("overlay").addEventListener("click", function () {
+  var videoContainer = document.getElementById("video-container");
+  videoContainer.innerHTML =
+    '<iframe src="https://www.youtube.com/embed/m6pTbEz4w3o?autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="width: 100%; height: 100%;"></iframe>';
+  videoContainer.style.display = "flex";
+  videoContainer.style.justifyContent = "center";
+  videoContainer.style.alignItems = "center";
+  videoContainer.style.height = "80vh";
 
-  var iframe = document.createElement("iframe");
-  iframe.setAttribute(
-    "src",
-    "https://www.youtube.com/embed/YOUR_VIDEO_ID?autoplay=1"
-  );
-  iframe.setAttribute("frameborder", "0");
-  iframe.setAttribute(
-    "allow",
-    "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-  );
-  iframe.setAttribute("allowfullscreen", true);
+  var backgroundContainer = document.getElementById("background-container");
+  backgroundContainer.style.display = "none";
+});
 
-  thumbnail.style.display = "none";
-  wrapper.appendChild(iframe);
-}
+document.querySelectorAll(".nav-item").forEach(function (menuItem) {
+  menuItem.addEventListener("mouseenter", function () {
+    var dropdown = menuItem.querySelector(".dropdown");
+    if (dropdown) {
+      dropdown.style.display = "flex";
+    }
+  });
+
+  menuItem.addEventListener("mouseleave", function () {
+    var dropdown = menuItem.querySelector(".dropdown");
+    if (dropdown) {
+      dropdown.style.display = "none";
+    }
+  });
+});
+
+document.querySelectorAll(".dropdown").forEach(function (dropdown) {
+  dropdown.addEventListener("mouseenter", function () {
+    this.style.display = "flex";
+  });
+
+  dropdown.addEventListener("mouseleave", function () {
+    this.style.display = "none";
+  });
+});
